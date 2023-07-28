7cf8d2bc4b5b9b9da6dd98db1b8c5f2671b4a9e7
cherrynoize <cherrynoize9987@outlook.com>
2023-07-28 16:48:04 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/* _____                      __
+  / ___/_________  ________  / /_____  ___  ____
+  \__ \/ ___/ __ \/ ___/ _ \/ //_/ _ \/ _ \/ __ \ authors ~////
+ ___/ / /__/ /_/ / /  /  __/ ,< /  __/  __/ /_/ / serafinomb
+/____/\___/\____/_/   \___/_/|_|\___/\___/ .___/  cherrynoize
+                                          /_/ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
